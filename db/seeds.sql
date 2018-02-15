afa991e8ae1eb52aeaeccb1aa13935e7547f912f
Giovanni Pardo <giovanni_pardo@utexas.edu>
2018-02-15 15:47:56 -0600

    make it better

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BigMac', 0);
